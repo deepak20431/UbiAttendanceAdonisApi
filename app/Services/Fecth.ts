5524851a54a109677f493c3c4a7433ec1c3cc71b
poornima batham <pbatham21@gmail.com>
2023-07-13 12:34:39 +0530

    ohk

--- app/Services/Fecth.ts
+++ app/Services/Fecth.ts
@@ -1,89 +1,61 @@
-
 import Database from "@ioc:Adonis/Lucid/Database";
-import  moment from "moment";
-export default  class ServiceNameService{
-  
+import moment from "moment";
+export default class ServiceNameService {
+  static async Fecth(data) {
+    // return data
+    let query: any = Database.from("AttendanceMainMaster")
+      .innerJoin(
+        "AttendanceChildMaster",
+        "AttendanceMainMaster.Id",
+        "AttendanceChildMaster.Id"
+      )
+      .innerJoin('onDeleteShift', 'table2.column', 'table3.column')
 
-    static async Fecth(MainData){
-  // return MainData.Id
-  // 
+      .where("OrganizationId", data.OrganizationId) 
 
-const originalDateString = MainData.AttendanceDate
-// Create a new Date object with the original date string
-const originalDate = new Date(originalDateString);
+  .innerJoin('table4', 'table3.column', 'table4.column')
+  .select('table1.*', 'table2.*', 'table3.*', 'table4.*')
+  .where('table1.someColumn', 'someValue')
+  .where('table2.someColumn', 'someValue')
+  .where('table3.someColumn', 'someValue')
+  .where('table4.someColumn', 'someValue')
+      .select("*");
 
-// Get the year, month, and day components from the Date object
-const year = originalDate.getFullYear();
-const month = (originalDate.getMonth() + 1).toString().padStart(2, '0');
-const day = originalDate.getDate().toString().padStart(2, '0');
+    if (data.AttendanceDate == undefined) {
+      let currDate = moment().format("YYYY-MM-DD");
+      query = query.where("AttendanceDate", currDate);
+    } else {
+      const originalDateString = data.AttendanceDate;
+      const originalDate = new Date(originalDateString);
+      const year = originalDate.getFullYear();
+      const month = (originalDate.getMonth() + 1).toString().padStart(2, "0");
+      const day = originalDate.getDate().toString().padStart(2, "0");
+      const formattedDate = `${year}-${month}-${day}`;
+      query = query.where("AttendanceDate", formattedDate);
+    }
 
-// Create the new date string in the desired format (YYYY-MM-DD)l
-const formattedDate = `${year}-${month}-${day}`;
+    const resp: any[] = [];
+    const queryResult = await query;
 
-
-
-
-
-      
-  let res:any[]=[];
-      let data:any[]=[];
-
-      const limit = 3; // Number of records to retrieve
-      const start = 0; // Starting index of the range
-      const end = 10;
-      let query:any =""
-      let q1 = '';
-      let q2 = '';
-
-
-      if ( MainData.Id !== 0) {
-        q1 += ` andwhere Id='${MainData.Id}'`;
-
-      }
-    
-      
-      if ( MainData.Id !== 0) {
-        q2 += ` andwhere Id='${MainData.Id}'`;
-
-      }
-    
-      
-      
-
-         const sql = await Database.from('AttendanceMainMaster')
-        .innerJoin('AttendanceChildMaster','AttendanceMainMaster.Id','AttendanceChildMaster.Id')
-        .select('*')
-        .andWhere('AttendanceChildMaster.OrganizationId',MainData.OrganizationId)
-        .whereRaw('AttendanceChildMaster.Id = ?', [q1])
-
-
- 
-        return sql
-
-
-    
-    // query.forEach( function(value){
-
-    //   data['Id'] =  value.Id; 
-    //   data['TimeIn'] =  value.TimeIn;
-    //   data['TimeOut'] =  value.TimeOut;  
-    //   data['Overtime'] =  value.Overtime;  
-    //   data['device'] = value.device;
-    //   data["AttendanceDate"] = new Date(value.AttendanceDate).toLocaleString("en-IN", { timeZone: "Asia/Kolkata" });;
-
-
-    
-
-    //   data['EmployeeId'] = value.EmployeeId;
-
- 
-    //   res.push(data['Id'],data['TimeIn'],data['TimeOut'],data['Overtime'],data['device'],data['AttendanceDate'],data['EmployeeId'] )        
-      
-
-    //   })
-    //   return res
-
-
-}
-
+    queryResult.forEach(function (val) {
+      const data: any = {};
+      data["Id"] = val.Id;
+      data["EmployeeId"] = val.EmployeeId;
+      data["EntryImage"] = val.EntryImage;
+      data["TimeIn"] = val.TimeIn;
+      data["TimeOut"] = val.TimeOut;
+      data["AttendanceDate"] = new Date(val.AttendanceDate).toLocaleString(
+        "en-IN",
+        { timeZone: "Asia/Kolkata" }
+      );
+      data["OrganizationId "] = val.OrganizationId;
+      data["OwnerId"] = val.OwnerId;
+      data["Overtime"] = val.Overtime;
+      data["TimeInEditStatus"] = val.TimeInEditStatus;
+      data["TimeOutEditStatus"] = val.TimeOutEditStatus;
+      data["device"] = val.device;
+      resp.push(data);
+    });
+    return resp;
+  }
 }
