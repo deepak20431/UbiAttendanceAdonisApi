c55f1457f56e9a8c456f93654952edc17ac8f0e5
omchauhan2 <omc0784@gmail.com>
2023-07-26 09:57:08 +0530

    26july

--- UbiAttendanceAdonisApi/app/Services/AttendanceService.ts
+++ UbiAttendanceAdonisApi/app/Services/AttendanceService.ts
@@ -264,5 +264,69 @@ export default class AttendanceService {
     //  res1.push(data1)
 
   }
+
+  public static async getdepartment(data) {
+
+    const query = await Database.from('DepartmentMaster').select('Id', Database.raw(`if(LENGTH("Name") > 30, concat(SUBSTR("Name", 1, 30), '....'), Name) as Name ,'archive'`)).where('OrganizationId', data.OrganizationId).orderBy('Name');
+
+    var res: any[] = [];
+
+    query.forEach((row) => {
+      var data = {};
+      data['Id'] = row.Id;
+      data['Name'] = row.Name;
+      data['archive'] = row.archive;
+      res.push(data)
+    })
+    return res;
+
+  }
+
+  public static async addDept(data) {
+
+    var currentdate = new Date();
+
+    const query = await Database.from('DepartmentMaster').where('Name', data.Name).andWhere('OrganizationId', data.OrganizationId);
+
+    if (query.length > 0) {
+      return false;
+    }
+    const query1 = await Database.insertQuery().table('DepartmentMaster').insert({
+      Name: data.Name,
+      OrganizationId: data.OrganizationId,
+      CreatedDate: currentdate,
+      CreatedById: data.CreatedById,
+      LastModifiedDate: currentdate,
+      LastModifiedById: data.LastModifiedById,
+      OwnerId: data.OwnerId,
+      archive: data.archive
+    });
+
+    return query1;
+
+
+  }
+
+  public static async updatedept(data) {
+
+    var currentdate = new Date();
+
+    const query = await Database.from('DepartmentMaster').select('Id', 'OrganizationId', 'Name').where('Id', data.Id).andWhere('OrganizationId', data.OrganizationId).andWhere('Name', data.Name);
+    if (query.length > 0) {
+      return false;
+    }
+
+
+    const query2 = await Database.query().from('DepartmentMaster').where('Id', data.Id)
+      .update({
+        Name: data.Name,
+        LastModifiedDate: currentdate,
+        LastModifiedById: data.LastModifiedById,
+        archive: data.archive
+      });
+
+    return query2;
+  }
 }
+
 
