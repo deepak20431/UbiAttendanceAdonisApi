b2724de52903b3a82e747f4faff39460144275a6
Ashish <ashish@ubitechsolutions.com>
2023-07-27 19:54:05 +0530

    login

---
+++ app/Validators/loginValidator.ts
@@ -0,0 +1,22 @@
+import { schema, CustomMessages } from '@ioc:Adonis/Core/Validator'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import BaseValidator from './BaseValidator'
+
+export default class loginValidator  extends BaseValidator{
+  constructor(protected ctx: HttpContextContract) {
+    super()
+
+  }
+
+    
+    static loginV ={
+  
+   schema:schema.create({
+    userName: schema.string.optional(),
+    password: schema.string.optional(),
+    loginMethod:schema.string.optional(),
+    OrgId:schema.number.optional()
+    
+    })
+  }
+}
--- start/routes.ts
+++ start/routes.ts
@@ -23,3 +23,6 @@ import Route from '@ioc:Adonis/Core/Route'
 Route.get('/fetch','DesigantionsController.retreiveDesign')
 Route.post('/add','DesigantionsController.AddDesign')
 Route.put('/update','DesigantionsController.UpdateDesign')
+
+
+Route.get('/login','loginApiController.checkLogin')
