7dfa3ede686e152e5e7ae08e4cb40dce69076aaf
ashee19980721 <asheejainubitechsolutions.com@gmail.com>
2023-07-29 17:01:01 +0530

    api done ashi

--- app/Controllers/Http/UserSettingsController.ts
+++ app/Controllers/Http/UserSettingsController.ts
@@ -1,3 +1,13 @@
-// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
+import UserSettingService from 'App/Services/UserSettingService';
+import UsersettingValidator from 'App/Validators/UserSettingValidator';
 
-export default class UserSettingsController {}
+
+export default class UsersettingsController {
+  public async UpdatePass({request,response}: HttpContextContract) {
+     await request.validate(UsersettingValidator.usersetting)
+     const api = await UserSettingService.changepassword(request.all())
+     return response.json(api);
+
+  }
+}
--- app/Helper/Helper.ts
+++ app/Helper/Helper.ts
@@ -1,7 +1,11 @@
 export default class Helper{
 
-    public static async getName(a:any)
+    public static async encode5t(str:any)
     {
-        return a
+        for (let i = 0; i < 5; i++) {
+            str = Buffer.from(str).toString('base64');
+            str = str.split('').reverse().join('');
+          }
+          return str;
     }
 }
