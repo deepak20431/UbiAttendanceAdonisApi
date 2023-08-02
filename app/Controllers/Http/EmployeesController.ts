0e1469163abe68365194bb8009eb5be9d6653823
deepak20431 <deepakubitechsolution98@gmail.com>
2023-08-02 10:33:34 +0530

    add login-code

--- app/Controllers/Http/EmployeesController.ts
+++ app/Controllers/Http/EmployeesController.ts
@@ -3,16 +3,16 @@ import EmployeeValidator from 'App/Validators/EmployeeValidator';
 import EmployeeService from 'App/Services/EmployeeService';
 
 export default class EmployeesController {
-    private data:any=[];
-        public async emplist({request,response}: HttpContextContract){   
-           const validation = await request.validate(EmployeeValidator.empvalid)
-           this.data['orgid']=validation.refno;
-           this.data['adminid']=validation.empid;
-           this.data['status']= validation.status?validation.status:1;
-           this.data['currentPage']=validation.currentPage?validation.currentPage:0;
-           this.data['perpage']=validation.perpage?validation.perpage:10;
-           this.data['searchval']=validation.searchval?validation.searchval:'';
-           const result = await EmployeeService.prototype.Employeelist(this.data);
-           return response.json(result);             
-        }
+        private data: any = [];
+        public async emplist({ request, response }: HttpContextContract) {
+                const validation = await request.validate(EmployeeValidator.empvalid)
+                this.data['orgid'] = validation.refno;
+                this.data['adminid'] = validation.empid;
+                this.data['status'] = validation.status ? validation.status : 1;
+                this.data['currentPage'] = validation.currentPage ? validation.currentPage : 0;
+                this.data['perpage'] = validation.perpage ? validation.perpage : 10;
+                this.data['searchval'] = validation.searchval ? validation.searchval : '';
+                const result = await EmployeeService.prototype.Employeelist(this.data);
+                return response.json(result);
+        }
 }
--- app/Routes/SignInRoute.ts
+++ app/Routes/SignInRoute.ts
@@ -1,6 +1,6 @@
 import Route from '@ioc:Adonis/Core/Route'
 
-Route.post('/login','LoginController.checkLogin').middleware('throttle:global').middleware('auth')
+Route.post('/login','LoginController.checkLogin').middleware('throttle:global')
 
 Route.put('/logout','LogoutsController.logout').middleware('throttle:global').middleware('auth')
 
